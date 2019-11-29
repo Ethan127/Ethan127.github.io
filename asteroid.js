fd6644326a18684d9bc6e9f2a2ff66df0e1ad160
Ethan <ethan.s127@gmail.com>
2019-11-28 20:00:21 -0700

    Fixed height/width

--- asteroid.js
+++ asteroid.js
@@ -57,7 +57,7 @@ function partialSetup(){
 }
 
 function setup() { 
-  createCanvas(960*2, 540*2, WEBGL); 
+  createCanvas(windowWidth, windowHeight, WEBGL); 
   noStroke();
   rectMode(CENTER);
   
