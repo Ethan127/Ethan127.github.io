66ecde367378f75b507926178bbc79b34855a6e7
Ethan <ethan.s127@gmail.com>
2019-11-28 21:12:04 -0700

    a

--- asteroid.js
+++ asteroid.js
@@ -203,10 +203,10 @@ function draw() {
   var s5 = "Your high score is " + round(Record());
   textSize(15);
   fill(255,255,0);
-  text(s1, 25-width/2, 25-width/2); 
-  text(s2, 350-width/2, 25-width/2); 
-  text(s3, 700-width/2, 25-width/2); 
-  text(s4, 300-width/2, 55-width/2);
+  text(s1, 25-width/2, 25-height/2); 
+  text(s2, 350-width/2, 25-height/2); 
+  text(s3, 700-width/2, 25-height/2); 
+  text(s4, 300-width/2, 55-height/2);
   fill(66, 200, 244);
   rectMode(CENTER);
   text(s5, 0, height/2, 180, 50);
